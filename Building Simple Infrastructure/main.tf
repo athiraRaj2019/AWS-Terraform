9960f9d23b4535419155d7100eff2705129da7b0
athiraRaj2019 <49579148+athiraRaj2019@users.noreply.github.com>
2021-05-03 10:05:14 +0530

    Update main.tf

--- Building Simple Infrastructure/main.tf
+++ Building Simple Infrastructure/main.tf
@@ -1,1 +1,14 @@
-fff
+provider "aws" {
+access_key = "<aws-access-key>"
+secret_key = "<aws-secret-key>"
+region = "us-east-1"
+  }
+
+resource "aws-instance" "terraform_demo"{
+  ami = "<ami name>"
+  instance type = "t2.micro"
+  key_name = "****"
+  tags{
+    Name = "Terraform-Server"
+    }
+  }
